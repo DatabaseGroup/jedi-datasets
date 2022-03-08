e7455954ff8bdb2adc7597ec18f2390124e7531f
Thomas Huetter <thomas.huetter@stud.sbg.ac.at>
2022-03-08 08:03:25 +0100

    updated scripts to python3

--- scripts/download-prepare.sh
+++ scripts/download-prepare.sh
@@ -91,73 +91,73 @@ mkdir -p input-data/virus
 # Convert the raw JSON data into the bracket notation input data format. Sort 
 # the sibling order to apply the JediOrder upper bound on ordered siblings.
 echo " * Processing arxiv ...\c"
-python scripts/json2bracket.py -f raw-data/arxiv/arxiv.json -c -s > input-data/arxiv/arxiv.bracket
+python3 scripts/json2bracket.py -f raw-data/arxiv/arxiv.json -c -s > input-data/arxiv/arxiv.bracket
 echo " Done"
 echo " * Processing cards ...\c"
-python scripts/json2bracket.py -f raw-data/cards/cards.json -c -s > input-data/cards/cards.bracket
+python3 scripts/json2bracket.py -f raw-data/cards/cards.json -c -s > input-data/cards/cards.bracket
 echo " Done"
 echo " * Processing clothing ...\c"
-python scripts/json2bracket.py -f raw-data/clothing/clothing.json -c -s > input-data/clothing/clothing.bracket
+python3 scripts/json2bracket.py -f raw-data/clothing/clothing.json -c -s > input-data/clothing/clothing.bracket
 echo " Done"
 echo " * Processing dblp ...\c"
-python scripts/json2bracket.py -f raw-data/dblp/dblp.json -c -s > input-data/dblp/dblp.bracket
+python3 scripts/json2bracket.py -f raw-data/dblp/dblp.json -c -s > input-data/dblp/dblp.bracket
 echo " Done"
 echo " * Processing denf ...\c"
-python scripts/json2bracket.py -f raw-data/denf/denf.json -c -s > input-data/denf/denf.bracket
+python3 scripts/json2bracket.py -f raw-data/denf/denf.json -c -s > input-data/denf/denf.bracket
 echo " Done"
 echo " * Processing device ...\c"
-python scripts/json2bracket.py -f raw-data/device/device.json -c -s > input-data/device/device.bracket
+python3 scripts/json2bracket.py -f raw-data/device/device.json -c -s > input-data/device/device.bracket
 echo " Done"
 echo " * Processing face ...\c"
-python scripts/json2bracket.py -f raw-data/face/face.json -c -s > input-data/face/face.bracket
+python3 scripts/json2bracket.py -f raw-data/face/face.json -c -s > input-data/face/face.bracket
 echo " Done"
 echo " * Processing fenf ...\c"
-python scripts/json2bracket.py -f raw-data/fenf/fenf.json -c -s > input-data/fenf/fenf.bracket
+python3 scripts/json2bracket.py -f raw-data/fenf/fenf.json -c -s > input-data/fenf/fenf.bracket
 echo " Done"
 echo " * Processing movies ...\c"
-python scripts/json2bracket.py -f raw-data/movies/movies.json -c -s > input-data/movies/movies.bracket
+python3 scripts/json2bracket.py -f raw-data/movies/movies.json -c -s > input-data/movies/movies.bracket
 echo " Done"
 echo " * Processing nasa ...\c"
-python scripts/json2bracket.py -f raw-data/nasa/nasa.json -c -s > input-data/nasa/nasa.bracket
+python3 scripts/json2bracket.py -f raw-data/nasa/nasa.json -c -s > input-data/nasa/nasa.bracket
 echo " Done"
 echo " * Processing nba ...\c"
-python scripts/json2bracket.py -f raw-data/nba/nba.json -c -s > input-data/nba/nba.bracket
+python3 scripts/json2bracket.py -f raw-data/nba/nba.json -c -s > input-data/nba/nba.bracket
 echo " Done"
 echo " * Processing reads ...\c"
-python scripts/json2bracket.py -f raw-data/reads/reads.json -c -s > input-data/reads/reads.bracket
+python3 scripts/json2bracket.py -f raw-data/reads/reads.json -c -s > input-data/reads/reads.bracket
 echo " Done"
 echo " * Processing recipes ...\c"
-python scripts/json2bracket.py -f raw-data/recipes/recipes.json -c -s > input-data/recipes/recipes.bracket
+python3 scripts/json2bracket.py -f raw-data/recipes/recipes.json -c -s > input-data/recipes/recipes.bracket
 echo " Done"
 echo " * Processing reddit ...\c"
-python scripts/json2bracket.py -f raw-data/reddit/reddit.json -c -s > input-data/reddit/reddit.bracket
+python3 scripts/json2bracket.py -f raw-data/reddit/reddit.json -c -s > input-data/reddit/reddit.bracket
 echo " Done"
 echo " * Processing schema ...\c"
-python scripts/json2bracket.py -f raw-data/schema/schema.json -c -s > input-data/schema/schema.bracket
+python3 scripts/json2bracket.py -f raw-data/schema/schema.json -c -s > input-data/schema/schema.bracket
 echo " Done"
 echo " * Processing smsen ...\c"
-python scripts/json2bracket.py -f raw-data/smsen/smsen.json -c -s > input-data/smsen/smsen.bracket
+python3 scripts/json2bracket.py -f raw-data/smsen/smsen.json -c -s > input-data/smsen/smsen.bracket
 echo " Done"
 echo " * Processing smszh ...\c"
-python scripts/json2bracket.py -f raw-data/smszh/smszh.json -c -s > input-data/smszh/smszh.bracket
+python3 scripts/json2bracket.py -f raw-data/smszh/smszh.json -c -s > input-data/smszh/smszh.bracket
 echo " Done"
 echo " * Processing spotify ...\c"
-python scripts/json2bracket.py -f raw-data/spotify/spotify.json -c -s > input-data/spotify/spotify.bracket
+python3 scripts/json2bracket.py -f raw-data/spotify/spotify.json -c -s > input-data/spotify/spotify.bracket
 echo " Done"
 echo " * Processing standev ...\c"
-python scripts/json2bracket.py -f raw-data/standev/standev.json -c -s > input-data/standev/standev.bracket
+python3 scripts/json2bracket.py -f raw-data/standev/standev.json -c -s > input-data/standev/standev.bracket
 echo " Done"
 echo " * Processing stantrain ...\c"
-python scripts/json2bracket.py -f raw-data/stantrain/stantrain.json -c -s > input-data/stantrain/stantrain.bracket
+python3 scripts/json2bracket.py -f raw-data/stantrain/stantrain.json -c -s > input-data/stantrain/stantrain.bracket
 echo " Done"
 echo " * Processing trees ...\c"
-python scripts/json2bracket.py -f raw-data/trees/trees.json -c -s > input-data/trees/trees.bracket
+python3 scripts/json2bracket.py -f raw-data/trees/trees.json -c -s > input-data/trees/trees.bracket
 echo " Done"
 echo " * Processing twitter2 ...\c"
-python scripts/json2bracket.py -f raw-data/twitter2/twitter2.json -c -s > input-data/twitter2/twitter2.bracket
+python3 scripts/json2bracket.py -f raw-data/twitter2/twitter2.json -c -s > input-data/twitter2/twitter2.bracket
 echo " Done"
 echo " * Processing virus ...\c"
-python scripts/json2bracket.py -f raw-data/virus/virus.json -c -s > input-data/virus/virus.bracket
+python3 scripts/json2bracket.py -f raw-data/virus/virus.json -c -s > input-data/virus/virus.bracket
 echo " Done"
 
 # Create input data directories in case they do not exist.
@@ -166,73 +166,73 @@ mkdir -p analysis/
 
 # Analyze datasets.
 echo " * Analyze arxiv ...\c"
-python scripts/analyze-json.py -f raw-data/arxiv/arxiv.json > analysis/arxiv.txt
+python3 scripts/analyze-json.py -f raw-data/arxiv/arxiv.json > analysis/arxiv.txt
 echo " Done"
 echo " * Analyze cards ...\c"
-python scripts/analyze-json.py -f raw-data/cards/cards.json > analysis/cards.txt
+python3 scripts/analyze-json.py -f raw-data/cards/cards.json > analysis/cards.txt
 echo " Done"
 echo " * Analyze clothing ...\c"
-python scripts/analyze-json.py -f raw-data/clothing/clothing.json > analysis/clothing.txt
+python3 scripts/analyze-json.py -f raw-data/clothing/clothing.json > analysis/clothing.txt
 echo " Done"
 echo " * Analyze dblp ...\c"
-python scripts/analyze-json.py -f raw-data/dblp/dblp.json > analysis/dblp.txt
+python3 scripts/analyze-json.py -f raw-data/dblp/dblp.json > analysis/dblp.txt
 echo " Done"
 echo " * Analyze denf ...\c"
-python scripts/analyze-json.py -f raw-data/denf/denf.json > analysis/denf.txt
+python3 scripts/analyze-json.py -f raw-data/denf/denf.json > analysis/denf.txt
 echo " Done"
 echo " * Analyze device ...\c"
-python scripts/analyze-json.py -f raw-data/device/device.json > analysis/device.txt
+python3 scripts/analyze-json.py -f raw-data/device/device.json > analysis/device.txt
 echo " Done"
 echo " * Analyze face ...\c"
-python scripts/analyze-json.py -f raw-data/face/face.json > analysis/face.txt
+python3 scripts/analyze-json.py -f raw-data/face/face.json > analysis/face.txt
 echo " Done"
 echo " * Analyze fenf ...\c"
-python scripts/analyze-json.py -f raw-data/fenf/fenf.json > analysis/fenf.txt
+python3 scripts/analyze-json.py -f raw-data/fenf/fenf.json > analysis/fenf.txt
 echo " Done"
 echo " * Analyze movies ...\c"
-python scripts/analyze-json.py -f raw-data/movies/movies.json > analysis/movies.txt
+python3 scripts/analyze-json.py -f raw-data/movies/movies.json > analysis/movies.txt
 echo " Done"
 echo " * Analyze nasa ...\c"
-python scripts/analyze-json.py -f raw-data/nasa/nasa.json > analysis/nasa.txt
+python3 scripts/analyze-json.py -f raw-data/nasa/nasa.json > analysis/nasa.txt
 echo " Done"
 echo " * Analyze nba ...\c"
-python scripts/analyze-json.py -f raw-data/nba/nba.json > analysis/nba.txt
+python3 scripts/analyze-json.py -f raw-data/nba/nba.json > analysis/nba.txt
 echo " Done"
 echo " * Analyze reads ...\c"
-python scripts/analyze-json.py -f raw-data/reads/reads.json > analysis/reads.txt
+python3 scripts/analyze-json.py -f raw-data/reads/reads.json > analysis/reads.txt
 echo " Done"
 echo " * Analyze recipes ...\c"
-python scripts/analyze-json.py -f raw-data/recipes/recipes.json > analysis/recipes.txt
+python3 scripts/analyze-json.py -f raw-data/recipes/recipes.json > analysis/recipes.txt
 echo " Done"
 echo " * Analyze reddit ...\c"
-python scripts/analyze-json.py -f raw-data/reddit/reddit.json > analysis/reddit.txt
+python3 scripts/analyze-json.py -f raw-data/reddit/reddit.json > analysis/reddit.txt
 echo " Done"
 echo " * Analyze schema ...\c"
-python scripts/analyze-json.py -f raw-data/schema/schema.json > analysis/schema.txt
+python3 scripts/analyze-json.py -f raw-data/schema/schema.json > analysis/schema.txt
 echo " Done"
 echo " * Analyze smsen ...\c"
-python scripts/analyze-json.py -f raw-data/smsen/smsen.json > analysis/smsen.txt
+python3 scripts/analyze-json.py -f raw-data/smsen/smsen.json > analysis/smsen.txt
 echo " Done"
 echo " * Analyze smszh ...\c"
-python scripts/analyze-json.py -f raw-data/smszh/smszh.json > analysis/smszh.txt
+python3 scripts/analyze-json.py -f raw-data/smszh/smszh.json > analysis/smszh.txt
 echo " Done"
 echo " * Analyze spotify ...\c"
-python scripts/analyze-json.py -f raw-data/spotify/spotify.json > analysis/spotify.txt
+python3 scripts/analyze-json.py -f raw-data/spotify/spotify.json > analysis/spotify.txt
 echo " Done"
 echo " * Analyze standev ...\c"
-python scripts/analyze-json.py -f raw-data/standev/standev.json > analysis/standev.txt
+python3 scripts/analyze-json.py -f raw-data/standev/standev.json > analysis/standev.txt
 echo " Done"
 echo " * Analyze stantrain ...\c"
-python scripts/analyze-json.py -f raw-data/stantrain/stantrain.json > analysis/stantrain.txt
+python3 scripts/analyze-json.py -f raw-data/stantrain/stantrain.json > analysis/stantrain.txt
 echo " Done"
 echo " * Analyze trees ...\c"
-python scripts/analyze-json.py -f raw-data/trees/trees.json > analysis/trees.txt
+python3 scripts/analyze-json.py -f raw-data/trees/trees.json > analysis/trees.txt
 echo " Done"
 echo " * Analyze twitter2 ...\c"
-python scripts/analyze-json.py -f raw-data/twitter2/twitter2.json > analysis/twitter2.txt
+python3 scripts/analyze-json.py -f raw-data/twitter2/twitter2.json > analysis/twitter2.txt
 echo " Done"
 echo " * Analyze virus ...\c"
-python scripts/analyze-json.py -f raw-data/virus/virus.json > analysis/virus.txt
+python3 scripts/analyze-json.py -f raw-data/virus/virus.json > analysis/virus.txt
 echo " Done"
 
 elapsed=$(( SECONDS - start_time ))
